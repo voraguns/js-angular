0d37b6a8ecdf758e38f41a6b60436020bfe2079f
Voragun <voragun2543@gmail.com>
2022-10-19 18:10:35 +0700

    add component

--- js-angular/src/app/app.module.ts
+++ js-angular/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Love } from "./love";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Love
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [Love]
 })
 export class AppModule { }
---
+++ js-angular/src/app/love.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+
+@Component({
+   selector: "love",
+   template: "<button>Love</button><button>Hate</button>"
+})
+
+export class Love { }
